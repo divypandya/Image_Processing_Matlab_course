405f31e30286689bd078f85376c2f3fb920d7cd7
Divy Pandya <201751013@iiitvadodara.ac.in>
2018-12-07 21:45:46 +0530

    made some changes in two files

--- Basic_Mask_filters.m
+++ Basic_Mask_filters.m
@@ -1,6 +1,5 @@
 I=imread('cameraman.tif');
-figure (1)
-imshow(I);
+figure, imshow(I);
 
 %making a function handle which calculates 2d Convolution of 
 %kernal/mask and image and convert it
@@ -10,24 +9,20 @@ Conv2d=@(kernal,image) uint8(conv2(kernal,image));
 %low pass filtering ( image-blurring)
 lowPass1=[1 1 1;1 1 1;1 1 1]/9;
 I1=Conv2d(lowPass1,I);
-figure (2)
-imshow(I1);
+figure, imshow(I1);
 
 %high pass filtering (edge-detection)
 highPass=[-1 -1 -1;-1 8 -1;-1 -1 -1];
 I2=Conv2d(highPass,I);
-figure (3)
-imshow(I2);
+figure, imshow(I2);
 
 %Image-sharpen
 sharp=[0 -1 0;-1 5 -1;0 -1 0];
 I3=Conv2d(sharp,I);
-figure (4)
-imshow(I3);
+figure, imshow(I3);
 
 %Band-Pass filter i.e. lowPass1-lowPass2
 lowPass2=[0 4 0; 4 8 4;0 4 0]/24;
 BandPass=lowPass1-lowPass2;
 I4=Conv2d(BandPass,I);
-figure (5)
-imshow(I4);
+figure, imshow(I4);
--- IntensityTranformation.m
+++ IntensityTranformation.m
@@ -1,30 +1,25 @@
 f=imread('moon.tif'); % f is our input image
-figure (1)
-imshow(f);
+figure, imshow(f);
 
 %%**IMADJUST Function**%%
 g=imadjust(f,[0 1],[1 0],0.4);
 %for high_out > low_out (i.e here [1 0]) output image is negative image
-figure (2)
-imshow(g);
+figure, imshow(g);
 %"stretchlim" to use imadjust without specifying low_in and high_in
 low_high=stretchlim(f);
 disp(low_high);
 g1=imadjust(f,low_high,[1 0]);
-figure (3)
-imshow(g1);
+figure, imshow(g1);
 
 %%**Logarithmic Tranformation**%%
 % of the form g=c*log(1+f);
 g2=2*log(1+double(f));
 g2=im2uint8(mat2gray(g2));
-figure (4)
-imshow(g2);
+figure, imshow(g2);
 
 %%**Contrast-Stretching Transformation**%%
 % of the form s=1/(1+(m/r)^E)) where r is input intensity
 m=0.5;
 E=1.5;
 g3=im2uint8(mat2gray((1./(1+(m./double(f)).^E))));
-figure (5)
-imshow(g3);
+figure, imshow(g3);
