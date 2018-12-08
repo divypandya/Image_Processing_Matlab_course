679bb76ece4007fcdb87a4922cdb107abc60aaed
Divy Pandya <201751013@iiitvadodara.ac.in>
2018-12-08 18:31:49 +0530

    added a file on Linear Spatial filtering
    use of commands imfilter and fspecial

---
+++ Linear_Spatial_Filtering.m
@@ -0,0 +1,24 @@
+f=imread('cameraman.tif'); %input image
+figure, imshow(f);
+w=ones(3); %Mask or Kernal
+%%**Linear Spatial Filtering :- Correlation and Convolution**%%
+%imfilter to find convolution and correlation of f and w
+gconv=imfilter(f,w,'conv','replicate'); %calculates convolution of f and w
+figure, imshow(gconv);
+gcorr=imfilter(f,w,'corr','replicate');
+figure, imshow(gcorr);
+
+%%**Special Spatial Filters**%%
+%fspecial command generates a filter mask for e.g.
+f1=imread('moon.tif');
+figure, imshow(f1);
+ws=fspecial('laplacian');%generates laplacian mask
+wh=[1 1 1;1 -8 1;1 1 1]; %laplacian mask with -8 at its center
+g1=imfilter(double(f1),ws,'conv','replicate');
+g2=imfilter(double(f1),wh,'conv','replicate');
+figure, imshow(g1,[]); %plotting floating point filtered image
+%enhancement g=f(x,y)-c*laplacian(f(x,y));
+g3=double(f1)-g1; %enhancement of floating point image
+figure, imshow(uint8(g3));
+g4=double(f1)-g2;
+figure, imshow(uint8(g4));
