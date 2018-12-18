d7705122336f9f0579e8823932b65e6d0b76e397
Divy Pandya <201751013@iiitvadodara.ac.in>
2018-12-18 17:17:47 +0530

    some updates in linear_spatial_filtering

--- Linear_Spatial_Filtering.m
+++ Linear_Spatial_Filtering.m
@@ -1,11 +1,10 @@
 f=imread('cameraman.tif'); %input image
 figure, imshow(f);
 w=ones(3); %Mask or Kernal
-%%**Linear Spatial Filtering :- Correlation and Convolution**%%
 %imfilter to find convolution and correlation of f and w
-gconv=imfilter(f,w,'conv','replicate'); %calculates convolution of f and w
+gconv=imfilter(f,w,'conv','replicate'); %convolution of f and w
 figure, imshow(gconv);
-gcorr=imfilter(f,w,'corr','replicate');
+gcorr=imfilter(f,w,'corr','replicate'); %correlation of f and w
 figure, imshow(gcorr);
 
 %%**Special Spatial Filters**%%
@@ -16,7 +15,8 @@ ws=fspecial('laplacian');%generates laplacian mask
 wh=[1 1 1;1 -8 1;1 1 1]; %laplacian mask with -8 at its center
 g1=imfilter(double(f1),ws,'conv','replicate');
 g2=imfilter(double(f1),wh,'conv','replicate');
-figure, imshow(g1,[]); %plotting floating point filtered image
+figure, imshow(uint8(g1),[]); %plotting floating point filtered image
+figure, imshow(g2,[]);
 %enhancement g=f(x,y)-c*laplacian(f(x,y));
 g3=double(f1)-g1; %enhancement of floating point image
 figure, imshow(uint8(g3));
